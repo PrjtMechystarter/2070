86d4564ea0f7ae74b86bab4ebdb055d17fa0968b
PrjtMechystarter <turquoisewavestw@gmail.com>
2025-10-04 17:07:35 -0700

    Add files via upload

---
+++ style.css
@@ -0,0 +1,12 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+    line-height: 1.6;
+}
+nav a {
+    margin-right: 10px;
+}
+header img {
+    display: block;
+    margin-top: 10px;
+}
